ba16181615625c26a0c5cd9cb45ed94e33b91908
rails <mdubey@bestpeers.com>
2020-01-27 19:41:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Keypad from "./Keypad";
+import Output from "./Output";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+state={
+result:""
+};
+
+buttonPressed= buttonName =>{
+if(buttonName === "="){
+this.calculate()
 }
 
+else
+if(buttonName === "c"){
+this.reset()
+}
+
+else {
+ this.setState({result: this.state.result + buttonName});
+}
+}
+
+reset = () =>{
+this.setState({result:""})
+}
+
+calculate = () => {
+try {
+this.setState({
+result:(eval(this.state.result) ||  "") + "" });
+}
+ 	
+catch(e){
+this.setstate({
+result: "error"
+});
+}
+};
+
+render(){
+	return(
+		<div className="App">
+			<div classname="calc-body">
+				<Output result= {this.state.result}/>
+				<Keypad buttonPressed={this.buttonPressed}/>
+			</div>
+		</div>
+	);
+}
+}
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,92 @@
+/*import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Hello extends React.Component{
+	render(){
+		return(
+			<h2>HEYYYY</h2>
+		);
+	}
+}
+ReactDOM.render(<Hello />, document.getElementById('root'));*/
+
+/*import React, { Component } from 'react';
+class App extends Component {
+render()
+{
+return(
+<div className="App">
+<h1>Hello World!</h1>
+</div>
+);
+}
+}
+export default App;
+*/
+ 
+/*import React from 'react';
+import ReactDOM from 'react-dom';
+const myfirstelement = <h1>Hello React!</h1>
+ReactDOM.render(myfirstelement, document.getElementById('root'));
+*/
+
+
+/*import React from 'react';
+import ReactDOM from 'react-dom';
+class Car extends React.Component{
+render()
+{
+return <h2>I Am a {this.props.brand}!</h2>;
+}
+}
+class Garage extends React.Component{
+render(){
+return(
+<div>
+<h1>Who lives in my Garage?</h1>
+<Car brand="Ford"/>
+</div>
+);
+}
+}
+ReactDOM.render(<Garage />,document.getElementById('root'));*/
+
+
+/*import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: "",
+      model: "Mustang",
+      color: "red",
+      year: 1964
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h1>My {this.state.brand}</h1>
+        <p>
+          It is a {this.state.color}
+          {this.state.model}
+          from {this.state.year}.
+        </p>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Car />, document.getElementById('root'));*/
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+
+
